87baa495012ec4b4dc3b32e687923d6004dd6741
Mita anggareni <mitaanggraeni733@gmail.com>
2026-05-06 11:25:05 +0700

    test project

---
+++ android/app/src/main/kotlin/com/example/utd_store_mita/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.utd_store_mita
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("UTD Store - Mita")),
+      body: const Center(
+        child: Text("Project ETS MPL", style: TextStyle(fontSize: 24)),
+      ),
+    );
+  }
+}
